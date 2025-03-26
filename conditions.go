fc7c4886a428b529bb51056357cf699d549b6543
Wasit Mirani <44178287+wasitmirani@users.noreply.github.com>
2025-03-26 23:26:44 +0500

    update conditions statements

---
+++ conditions.go
@@ -0,0 +1,21 @@
+package main
+
+func main(){
+
+	println(("welcome to the conditions section"))
+
+	age  := 20;
+
+	if(age > 18){
+		println("You are an adult")
+	} else {
+		println("You are a minor")
+	}
+
+
+	if num := 20; num <= 18 {
+		println("You are an adult")
+	} else {
+		println("You are a minor")
+	}
+}
