ef8a35861ee5cf4476196f6165101335c5eec36d
Wasit Mirani <44178287+wasitmirani@users.noreply.github.com>
2025-04-24 00:31:29 +0500

    add defer fucntion

---
+++ defer.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("one main start")
+	 myDefer()
+	 myDefer2()
+	fmt.Println("two main end")
+	fmt.Println("three main end")
+}
+
+func myDefer() {
+	fmt.Println("------myDefer start-----")
+	defer func() {
+		fmt.Println("---myDefer end 01---")
+	}()
+	fmt.Println("---- myDefer end -----")
+}
+func myDefer2() {
+	fmt.Println("----myDefer2 start -----")
+	defer func() {
+		fmt.Println("myDefer2 end------")
+	}()
+
+	fmt.Println("---- myDefer2 end -----")
+}
