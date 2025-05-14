0a6ad78402d67efa57e85b0ba0d5b3ebd668fdba
Wasit Mirani <44178287+wasitmirani@users.noreply.github.com>
2025-05-15 00:45:56 +0500

    refactor: update HTTP request handling and improve URL parsing

--- arrays.go
+++ arrays.go
@@ -67,4 +67,5 @@ func sliceArray( ){
 
 )
 
+
 }
--- httprequest.go
+++ httprequest.go
@@ -4,18 +4,45 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 )
-const url="http://www.baidu.com";
+
+const endpoint="http://www.baidu.com";
 func main() {
 	fmt.Println("welcome to the web request of Go!")
-	getRequest()
+	// getRequest()
 	fmt.Println("welcome to the web request of Go!")
+	getURLData()
+	
 }
 
 
+func getURLData(){
+
+	result, err := url.Parse("http://www.baidu.com?query=golang&name=zhangsan#fragment")
+	if err != nil {
+		fmt.Println("Error:", err)
+		panic(err)
+		return
+	}
+	fmt.Println("Scheme:", result.Scheme)
+	fmt.Println("Host:", result.Host)
+	fmt.Println("Path:", result.Path)
+	fmt.Println("Raw Path:", result.RawPath)
+	fmt.Println("Raw Query:", result.RawQuery)
+	fmt.Println("Raw Fragment:", result.RawFragment)
+
+	fmt.Printf("Raw Port:", result.Port())
+	fmt.Println("Raw User:", result.User)
+	fmt.Println("Raw User Info:", result.User)
+
+	queryparanms := result.Query()
+	fmt.Printf("Query Parameters: %s", queryparanms["name"])
+
+}
 
 func getRequest() {
-	resp, err := http.Get(url)
+	resp, err := http.Get(endpoint)
 
 	if err != nil {
 		fmt.Println("Error:", err)
